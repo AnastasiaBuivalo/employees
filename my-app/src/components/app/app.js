a20021390bd58bd089f6f641e594b72e6f74a8dc
AnastasiaBuivalo <abuyvalo@mail.ru>
2022-10-27 14:04:39 +0300

    create project employees

---
+++ my-app/src/components/app/app.js
@@ -0,0 +1,20 @@
+import AppInfo from '../app-info/app-info';
+import AppFilter from '../app-filter/app-filter';
+import SearchPanel from '../search-panel/search-panel'
+import EmployeesList from '../ employees-list/ employees-list'
+import EmployeesAddForm from '../ employees-add-form/employees-add-form';
+import './app.css';
+
+function App(){
+    return <div className = 'app'>
+                <AppInfo />
+                <div className='search-panel'>
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                <EmployeesList/>
+                <EmployeesAddForm/>
+            </div>
+}
+
+export default App;
