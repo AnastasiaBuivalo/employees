b8d71049081c93482d26f4bf972c04aad45da950
AnastasiaBuivalo <abuyvalo@mail.ru>
2022-10-28 12:13:56 +0300

    btn coockie and star

--- my-app/src/components/ employees-list-item/ employees-list-item.js
+++ my-app/src/components/ employees-list-item/ employees-list-item.js
@@ -1,27 +1,57 @@
 import './ employees-list-item.css'
+import {Component} from 'react'
 
-const EmployeesListItem = ({name, salary, increase, id})=>{
-    let classNames = "list-group-item d-flex justify-content-between";
-    if (increase)
-        classNames += ' increase';
-    return (
-        <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+
+class EmployeesListItem extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            increase: false,
+            rise: false
+        }
+    };
+
+    onIncrease = ()=>{
+        this.setState(({increase})=>({
+            increase: !increase
+        }))
+    }
+
+    onRise = () => {
+        this.setState(({rise})=>({
+            rise: !rise
+        }))
+    }
+
+    render(){
+        let classNames = "list-group-item d-flex justify-content-between";
+        const {name, salary, id} = this.props;
+        const {increase, rise} = this.state;
+        if (increase)
+            classNames += ' increase';
+        if(rise)
+            classNames += ' like';
+        return (
+            <li className={classNames}>
+                <span className="list-group-item-label" onClick={this.onRise}>{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+
+                    <button type="button"
+                            className="btn-trash btn-sm ">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default EmployeesListItem;
--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -1,3 +1,4 @@
+import {Component} from 'react'
 import AppInfo from '../app-info/app-info';
 import AppFilter from '../app-filter/app-filter';
 import SearchPanel from '../search-panel/search-panel'
@@ -6,21 +7,51 @@ import EmployeesAddForm from '../ employees-add-form/employees-add-form';
 import './app.css';
 
 
-function WhoAmI({name, surname, link}){
-    return (
-        <div>
-            <h1> My name is {name}, surname - {surname}</h1>
-            <a href= {link}>My profile</a>
-        </div>
+class WhoAmI extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: 27,
+            text: '+++',
+            position: ''
+        }
+    };
 
-    )
+    nextYear = ()=>{
+        this.setState(state =>({
+            age: state.age+1
+        }))
+    }
+
+    commitInputChanges = (e, text)=>{
+        this.setState(() =>({
+            position: e.target.value
+        }))
+    }
+    
+    render(){
+        const {name, surname, link} = this.props;
+        const {age, position, text} = this.state;
+        return (
+            <div>
+                <h1> My name is {name}, surname - {surname},
+                 age - {age}
+                 position - {position}</h1>
+                <button onClick = {this.nextYear}>{text}</button>
+                <span>Должность: </span>
+                <input type="text" onChange={(e) => this.commitInputChanges(e, 'some change')}/>
+                <a href= {link}>My profile</a>
+            </div>
+    
+        )
+    }
 }
 
 const data = [
-    {name: 'Jo', salary: 800, increase: false, id: 1},
-    {name: 'Mike', salary: 1000, increase: true, id: 2},
-    {name: 'Nike', salary: 500, increase: true, id: 3},
-    {name: 'Bob', salary: 1800, increase: false, id: 4},
+    {name: 'Jo', salary: 800, id: 1},
+    {name: 'Mike', salary: 1000, id: 2},
+    {name: 'Nike', salary: 500, id: 3},
+    {name: 'Bob', salary: 1800, id: 4},
 ]
 
 function App(){
