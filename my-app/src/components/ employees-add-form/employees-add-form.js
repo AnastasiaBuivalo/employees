c24cfb6081f8fec558575dd62d1032c2027d7441
AnastasiaBuivalo <abuyvalo@mail.ru>
2022-10-28 23:10:02 +0300

    add incressed and count salaryees

--- my-app/src/components/ employees-add-form/employees-add-form.js
+++ my-app/src/components/ employees-add-form/employees-add-form.js
@@ -7,7 +7,8 @@ class EmployeesAddForm extends Component {
         super(props);
         this.state ={
             name: '',
-            salary: ''
+            salary: '', 
+            error: false
         }
     }
 
@@ -20,15 +21,26 @@ class EmployeesAddForm extends Component {
 
     onSubmit = (e)=> {
         e.preventDefault();
-        this.props.onAdd(this.state.name, this.state.salary);
-        this.setState({
-            name: '',
-            salary: ''
-        })
+        if(this.state.name.length > 1 && this.state.salary !== ''){
+            this.props.onAdd(this.state.name, this.state.salary);
+            this.setState({
+                name: '',
+                salary: '',
+                error: false
+            })
+        }
+        else{
+            alert('Ошибка ввода')
+            this.setState({
+                error: true
+            })
+        }
     }
 
     render(){
         const {name, salary} = this.state;
+        let className = "form-control new-post-label";
+        console.log(className);
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
@@ -36,13 +48,13 @@ class EmployeesAddForm extends Component {
                     className="add-form d-flex"
                     onSubmit={this.onSubmit}>
                     <input type="text"
-                        className="form-control new-post-label"
+                        className={className}
                         placeholder="Как его зовут?" 
                         value = {name}
                         name = "name"
                         onChange = {this.onValueChange}/>
                     <input type="number"
-                        className="form-control new-post-label"
+                        className={className}
                         placeholder="З/П в $?"
                         value = {salary}
                         name = "salary"
--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -33,7 +33,7 @@ class WhoAmI extends Component{
         const {name, surname, link} = this.props;
         const {age, position, text} = this.state;
         return (
-            <div>
+            <>
                 <h1> My name is {name}, surname - {surname},
                  age - {age}
                  position - {position}</h1>
@@ -41,7 +41,7 @@ class WhoAmI extends Component{
                 <span>Должность: </span>
                 <input type="text" onChange={(e) => this.commitInputChanges(e, 'some change')}/>
                 <a href= {link}>My profile</a>
-            </div>
+            </>
     
         )
     }
@@ -52,13 +52,13 @@ class App extends Component{
         super(props);
         this.state ={
             data: [
-                {name: 'Jo', salary: 800, id: 1},
-                {name: 'Mike', salary: 1000, id: 2},
-                {name: 'Nike', salary: 500, id: 3},
-                {name: 'Bob', salary: 1800, id: 4},
-            ],
-            maxId : 5
-        }
+                {name: 'Jo', salary: 800, increase: false, rise: false, id: 1},
+                {name: 'Mike', salary: 1000, increase: false, rise: false, id: 2},
+                {name: 'Nike', salary: 500, increase: false, rise: false, id: 3},
+                {name: 'Bob', salary: 1800, increase: false, rise: false, id: 4},
+            ]
+        };
+        this.maxId = 5;
     }
 
     deleteItem = (id =>{
@@ -69,33 +69,52 @@ class App extends Component{
         console.log(this.state.data);
     })
 
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    console.log(id, [prop]);
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+
     addItem = ((name, salary) =>{
         const item = {
             name:name, 
             salary:salary,
-            id: this.state.maxId++
+            increase: false,
+            rise: false,
+            id: this.maxId++
         };
         this.setState(({data}) => ({
             data: [...data, item]
         }))
     })
-    render(){
-    return (
-        <div className = 'app'>
-            <AppInfo />
-            <div className='search-panel'>
-                <SearchPanel/>
-                <AppFilter/>
-            </div>
-            <EmployeesList 
-                data = {this.state.data}
-                onDelete = {this.deleteItem}/>
-            <EmployeesAddForm
-                onAdd = {this.addItem}/>
 
-            <WhoAmI name = 'Jo' surname= 'Smith' link = 'vk.com'/>
-            <WhoAmI name = 'Mike' surname= 'Smith' link = 'facebook.com'/>
-        </div>)}
+    render(){
+        const employees = this.state.data.length;
+        const increased= this.state.data.filter(item => item.increase).length;
+        return (
+            <div className = 'app'>
+                <AppInfo employees={employees} increased = {increased}/>
+                <div className='search-panel'>
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                <EmployeesList 
+                    data = {this.state.data}
+                    onDelete = {this.deleteItem}
+                    onToggleProp = {this.onToggleProp}/>
+                <EmployeesAddForm
+                    onAdd = {this.addItem}/>
+
+                <WhoAmI name = 'Jo' surname= 'Smith' link = 'vk.com'/>
+                <WhoAmI name = 'Mike' surname= 'Smith' link = 'facebook.com'/>
+            </div>)
+        }
             
 };
 
